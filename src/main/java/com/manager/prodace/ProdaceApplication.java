c8d52c053a130738c0663d23de4f94b2f90cd3a7
Risheek Verma <95381269+risheekv@users.noreply.github.com>
2025-03-11 01:39:28 +0530

    Spring Initialized

---
+++ src/main/java/com/manager/prodace/ProdaceApplication.java
@@ -0,0 +1,13 @@
+package com.manager.prodace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/prodace/ProdaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.prodace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
